21238ea6b60cff24d55b3f4137e54a372366e55a
folaulau <folaukaveinga@gmail.com>
2019-12-24 00:48:03 -0800

    added two sum

--- leetcode/src/main/java/com/lovemesomecoding/_1TwoSum.java
+++ leetcode/src/main/java/com/lovemesomecoding/_1TwoSum.java
@@ -5,6 +5,9 @@ import java.util.HashMap;
 import java.util.Map;
 
 /*
+ * 
+ * Difficulty - Easy
+ * 
  * Given an array of integers, return indices of the two numbers such that 
  * they add up to a specific target. You may assume that each input would have 
  * exactly one solution, and you may not use the same element twice.
